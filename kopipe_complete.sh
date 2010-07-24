318c0b5a5b3a59661b637c1d1885ac869bf80eaa
Cairnarvon <cairnarvon@gmail.com>
2010-07-24 04:17:59 +0200

    bash_completion support.

---
+++ kopipe_complete.sh
@@ -0,0 +1,22 @@
+# bash auto-completion for kopipe
+# Put this file (or a link to it) in /etc/bash_completion.d/
+
+__kopipe()
+{
+    local cur
+
+    COMPREPLY=()
+    cur=`_get_cword`
+
+    if [[ "$cur" == -* ]]; then
+        COMPREPLY=( $( compgen -W \
+            '-h --help -l --list -n --new -e --edit -d --delete' \
+            -- $cur ) )
+    else
+        cd ~/.kopipe
+        COMPREPLY=( $( compgen -f -- $cur ) )
+        cd - >/dev/null
+    fi
+}
+complete -F __kopipe $filenames kopipe
+complete -F __kopipe $filenames kopipe.sh
